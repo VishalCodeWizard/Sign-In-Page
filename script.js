adccf01b7a40bddc63f086ff16323d50eda4a286
VishalCodeWizard <114295403+VishalCodeWizard@users.noreply.github.com>
2023-12-23 21:57:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Example JavaScript for handling the "Sign in with Email" button click event
+document.getElementById('signInEmail').addEventListener('click', function() {
+    alert('Implement your sign-in logic for Email here.');
+    // You would typically handle the sign-in logic, e.g., redirecting to a login page or making API calls
+});
+
+// Example JavaScript for handling the "Sign in with GitHub" button click event
+document.getElementById('signInGitHub').addEventListener('click', function() {
+    alert('Implement your sign-in logic for GitHub here.');
+    // You would typically handle the sign-in logic, e.g., redirecting to a login page or making API calls
+});
+
+// Example JavaScript for handling the "Sign in with LinkedIn" button click event
+document.getElementById('signInLinkedIn').addEventListener('click', function() {
+    alert('Implement your sign-in logic for LinkedIn here.');
+    // You would typically handle the sign-in logic, e.g., redirecting to a login page or making API
